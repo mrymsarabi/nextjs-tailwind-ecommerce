b75602f49dd364ad0dd135e975d75f25892c7da5
Mrymsarabi <mrymsarabi@gmail.com>
2025-08-30 20:58:18 +0330

    TextArea component - 1

---
+++ src/components/form/TextArea.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+interface TextAreaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+    label?: string;
+    containerClassName?: string;
+    labelClassName?: string;
+}
+
+const TextArea: React.FC<TextAreaProps> = ({
+    label,
+    name,
+    value,
+    onChange,
+    placeholder = "",
+    rows = 4,
+    containerClassName = "",
+    className = "",
+    labelClassName = "",
+    disabled = false,
+    required = false,
+    ...rest
+}) => {
+    return (
+        <div className={`${containerClassName} mb-3`}>
+        {label && (
+            <label htmlFor={name} className={`form-label ${labelClassName}`}>
+            {label} {required && "*"}
+            </label>
+        )}
+        <textarea
+            id={name}
+            name={name}
+            value={value}
+            onChange={onChange}
+            placeholder={placeholder}
+            rows={rows}
+            className={`form-control ${className}`}
+            disabled={disabled}
+            required={required}
+            {...rest} 
+        />
+        </div>
+    );
+};
+
+export default TextArea;
